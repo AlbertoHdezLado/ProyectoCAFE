28bed24696e6162d3c943e481b9ef2a795ecc20f
Alberto Hernández Lado <albertohljob@gmail.com>
2022-11-28 22:41:18 +0100

    Translator devuelve un documento al slot de salida pasado por parámetro

--- src/Tasks/Translator.java
+++ src/Tasks/Translator.java
@@ -2,8 +2,11 @@ package Tasks;
 
 import Utils.Slot;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathFactory;
@@ -14,14 +17,16 @@ import java.util.Queue;
 public class Translator {
 
     Slot inputSlot;
+    Slot outputSlot;
     String xPathExpression;
 
-    public Translator(Slot inputSlot, String xPathExpression) {
+    public Translator(Slot inputSlot, Slot outputSlot, String xPathExpression) {
         this.inputSlot = inputSlot;
+        this.outputSlot = outputSlot;
         this.xPathExpression = xPathExpression;
     }
 
-    public Queue<String> TranslateSQL(String variable, String TableName, String otherConditions) {
+    public void TranslateSQL(String variable, String TableName, String otherConditions) {
         Queue<String> BDQueryStringList = new LinkedList<>();
         while (!inputSlot.getQueue().isEmpty()) {
             Document inputDocument = inputSlot.dequeue();
@@ -32,13 +37,25 @@ public class Translator {
 
                 String element = node.item(0).getTextContent();
 
-                BDQueryStringList.add("select * from " + TableName + " where " + variable + "='" + element + "' " + otherConditions);
 
+
+                // Builder para cargar el documento
+                DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder builder = dbFactory.newDocumentBuilder();
+
+                // Creamos un documento
+                Document translatedDocument = builder.newDocument();
+
+                // Añadimos el nodo sql
+                Element sqlElement = translatedDocument.createElement("sql");
+                sqlElement.setTextContent("select * from " + TableName + " where " + variable + "='" + element + "' " + otherConditions);
+                translatedDocument.appendChild(sqlElement);
+
+                outputSlot.enqueue(translatedDocument);
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        return BDQueryStringList;
     }
 }
